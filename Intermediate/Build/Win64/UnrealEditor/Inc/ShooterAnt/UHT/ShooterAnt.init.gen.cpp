1cc5fc2db99f04b1b32d6a6253208f88b7afee97
Anthony Acosta <anthonycylecacosta@gmail.com>
2025-12-02 17:52:28 -0500

    set up the blueprint class for gun

--- Intermediate/Build/Win64/UnrealEditor/Inc/ShooterAnt/UHT/ShooterAnt.init.gen.cpp
+++ Intermediate/Build/Win64/UnrealEditor/Inc/ShooterAnt/UHT/ShooterAnt.init.gen.cpp
@@ -21,13 +21,13 @@ void EmptyLinkFunctionForGeneratedCodeShooterAnt_init() {}
 				SingletonFuncArray,
 				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0x72CC7540,
-				0xCFD9B40A,
+				0x46F55719,
+				0xC8835592,
 				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_ShooterAnt.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_ShooterAnt.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_ShooterAnt(Z_Construct_UPackage__Script_ShooterAnt, TEXT("/Script/ShooterAnt"), Z_Registration_Info_UPackage__Script_ShooterAnt, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x72CC7540, 0xCFD9B40A));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_ShooterAnt(Z_Construct_UPackage__Script_ShooterAnt, TEXT("/Script/ShooterAnt"), Z_Registration_Info_UPackage__Script_ShooterAnt, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x46F55719, 0xC8835592));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
